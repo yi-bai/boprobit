ae29d4337b16fe9bb0a6a0e80869e491618b75b4
Yi Bai <yibai.thu@gmail.com>
2017-03-12 21:09:48 +0900

    Change likelihood function, indentation fixed.

--- 20170312test.r
+++ 20170312test.r
@@ -39,61 +39,39 @@ ggplot(mydata, aes(x = x2, y = y2)) + geom_point()
 
 # The bivariate ordered probit function
 log.lik <- function(par , X1 , X2 , Y1 , Y2) {
-#X1 <- cbind(1,X1)
-X1 <- as.matrix(X1)
-X2 <- as.matrix(cbind(1,X2))
+	#X1 <- cbind(1,X1)
+	X1 <- as.matrix(X1)
+	X2 <- as.matrix(cbind(1,X2))
 
-Beta1 <- par[ncol(X1)]		#parameters for the first equation
-Beta2 <- par[(ncol(X1)+1):(ncol(X1)+2)]   #parameters for the second equation
+	Beta1 <- par[ncol(X1)]		#parameters for the first equation
+	Beta2 <- par[(ncol(X1)+1):(ncol(X1)+2)]   #parameters for the second equation
 
-cut1 <- par[ncol(X1)+3]	#Second cut point for the first equation
-cut2 <- par[ncol(X1)+4]
-#cut22 <- par[(ncol(X1)+ncol(X2)+2)]	#Second cut point for the second equation
+	cut1 <- par[ncol(X1)+3]	#Second cut point for the first equation
+	cut2 <- par[ncol(X1)+4]
+	#cut22 <- par[(ncol(X1)+ncol(X2)+2)]	#Second cut point for the second equation
 
-gamma <- par[ncol(X1)+5]	  #parameter for the correlation coefficient
+	gamma <- par[ncol(X1)+5]	  #parameter for the correlation coefficient
 
-#multiply gamma by a column of 1's, and then transform: (exp(rho)-1)/(1+exp(rho))
-rho <- (exp(gamma) - 1) / (1 + exp( gamma)) 
+	#multiply gamma by a column of 1's, and then transform: (exp(rho)-1)/(1+exp(rho))
+	rho <- (exp(gamma) - 1) / (1 + exp( gamma)) 
 
-mu1 <- as.matrix(X1) %*% Beta1 
-mu2 <- as.matrix(X2) %*% Beta2
+	mu1 <- as.matrix(X1) %*% Beta1 
+	mu2 <- as.matrix(X2) %*% Beta2
+	
+	llik <- 0 
 
-
-
-llik <- 0 
-
-for (i in 1:nrow(mu1)){
 	cut_y1 <- c(-10000, cut1, cut2, 10000)
 	cut_y2 <- c(-10000, 0, 10000)
-	
-	for
-	
-}
+	for (i in 1:nrow(mu1)){
+		Sigma <- matrix(c(1, rho, rho, 1), 2, 2) 
+		cut_left <- cut_y1[Y1[i]]
+		cut_right <- cut_y1[Y1[i]+1]
+		cut_down <- cut_y2[Y2[i]]
+		cut_up <- cut_y2[Y2[i]+1]
+		llik <- llik + log(pmvnorm(lower = c(cut_left, cut_down), upper = c(cut_right, cut_up), mean = c(mu1[i,], mu2[i,]), corr = Sigma))
+	}
 
-for (i in 1:nrow(mu1)){ 
-	Sigma <- matrix(c(1, rho, rho, 1), 2, 2) 
-  
-	if (Y1[i]==0){	#All the calculations if y1 = 0				
-		if (Y2[i]==0){    #Y1=0 and Y2=0
-			llik <- llik + log(pmvnorm(lower = c(-Inf, -Inf), upper = c(0, 0), mean = c((cut1 - mu1[i,]), 0 - mu2[i,]), corr = Sigma))
-		}else{	#Y1=0 and Y2=1
-			llik <- llik + log(pmvnorm(lower = c(-Inf, 0), upper = c(Inf, Inf), mean = c((cut1 - mu1[i,]),0), corr = Sigma) - pmvnorm(lower = c(-Inf, -Inf), upper = c(Inf, 0), mean = c((cut1 - mu1[i,]), -mu2[i,]), corr = Sigma))
-		}
-	}else if (Y1[i]==1){	#All the calculations if y1 = 1	
-		if (Y2[i]==0){  #y1=1 and y2=0
-			llik <- llik + log(pmvnorm(lower = c(-Inf, -Inf), upper = c(Inf, 0), mean = c((cut2 - mu1[i,]), -mu2[i,]), corr = Sigma) - pmvnorm(lower = c(-Inf, -Inf), upper = c(Inf, 0), mean = c((cut1 - mu1[i,]), -mu2[i,]), corr = Sigma))	
-		}else{	#y1=1 and y2=1
-			llik <- llik + log(pmvnorm(lower = c(-Inf, 0), upper = c(Inf, Inf), mean=c((cut2 - mu1[i,]), 0), corr = Sigma) - pmvnorm(lower = c(-Inf, 0), upper = c(Inf, Inf), mean = c((cut2 - mu1[i,]), -mu2[i,]), corr = Sigma) - pmvnorm(lower = c(-Inf, -Inf), upper = c(Inf, Inf), mean = c((cut1 - mu1[i,]), 0), corr = Sigma) + pmvnorm(lower = c(-Inf, -Inf), upper = c(Inf, 0), mean = c((cut1 - mu1[i,]), -mu2[i,]), corr = Sigma))
-		}					
-	}else{		#All the calculations if y1 = 2
-		if (Y2[i]==0){  #y1=2 and y2=0		
-			llik <- llik +  log(pmvnorm(lower = c(-Inf, -Inf), upper = c(Inf,0),mean=c(0,-mu2[i,]), corr = Sigma) - pmvnorm(lower = c(-Inf, -Inf), upper = c(Inf, 0), mean = c((cut2 - mu1[i,]), -mu2[i,]), corr = Sigma)) 	
-		}else{		#y1=2 and y2=1
-			llik <- llik + log(1 - pmvnorm(lower = c(-Inf, -Inf),upper = c(Inf, Inf), mean = c((cut2 - mu1[i,]), 0),corr = Sigma) + pmvnorm(lower = c(-Inf, -Inf), upper = c(Inf, 0), mean = c((cut2 - mu1[i,]), -mu2[i,]), corr = Sigma))
-		}
-    }
-	return(llik)}
-
+	return(llik)
 }
  
 
